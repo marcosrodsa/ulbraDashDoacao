ad7f836d1bed1cd93b4528311abc37a144f1de36
Marco <marcos.rodsa@gmail.com>
2026-04-30 20:47:21 -0300

    feat: configure Storybook 8 with ULBRA theme

---
+++ brand-book/.storybook/main.ts
@@ -0,0 +1,17 @@
+import type { StorybookConfig } from '@storybook/react-vite';
+
+const config: StorybookConfig = {
+  framework: '@storybook/react-vite',
+  stories: ['../src/**/*.stories.@(ts|tsx|js|jsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-a11y',
+    '@storybook/addon-interactions',
+  ],
+  docs: {
+    autodocs: true,
+  },
+};
+
+export default config;
